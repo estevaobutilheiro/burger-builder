cfd38ecda3c09796eb08316efeaf62a7833c8ded
Estevao <brreconning@hotmail.com>
2021-01-04 18:00:49 +0000

    commit on 'master'

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,11 @@ import React from 'react';
 import classes from './Button.css';
 
 const button = (props) => (
-    <button className={[classes.Button, classes[props.btnType]].join(' ')} onClick={props.clicked}>
+    <button 
+        disabled={props.disabled}
+        className={[classes.Button, classes[props.btnType]].join(' ')} 
+        onClick={props.clicked}
+        >
         {props.children}
     </button>
 )
